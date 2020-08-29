5abcfdcd0248beaee5fa7c9e37edde978cd19f9c
Santiago Agustin Laiton Cubides <santiago.laiton@mail@escuelaing.edu.co>
2020-08-29 09:50:14 -0500

    agregamos pruebas

--- src/main/java/hangman/model/OriginalScore.java
+++ src/main/java/hangman/model/OriginalScore.java
@@ -1,8 +1,18 @@
 package hangman.model;
 
 public class OriginalScore implements GameScore{
-	
+	private int score;
+	private int penalizacion;
+	private int minimo;
 	public int calculateScore(int correctCount, int incorrectCount) throws GameScoreExceptions {
+		score=100;
+		penalizacion=10;
+		minimo=0;
+		
+		if(score-(incorrectCount*penalizacion)<minimo) {
+			
+		}
+		
 		return 0;
 	}
 	
---
+++ src/test/java/hangman/model/GameScoreTest.java
@@ -0,0 +1,16 @@
+package hangman.model;
+
+import org.junit.Test;
+
+
+
+
+public class GameScoreTest {
+	/**
+	 * OriginalScore
+	 */
+	@Test
+	public void validateScore(){
+		
+	}
+}
