f78cbfb129036617add1fed69b88d59b8fa0f861
Santiago Agustin Laiton Cubides <santiago.laiton@mail@escuelaing.edu.co>
2020-08-29 09:01:43 -0500

    Especificaión Métodos

---
+++ src/main/java/hangman/model/OriginalScore.java
@@ -0,0 +1,10 @@
+package hangman.model;
+
+public class OriginalScore implements GameScore{
+	
+	public int calculateScore(int correctCount, int incorrectCount) throws GameScoreExceptions {
+		return 0;
+	}
+	
+
+}
---
+++ src/main/java/hangman/model/PowerScore.java
@@ -0,0 +1,9 @@
+package hangman.model;
+
+public class PowerScore implements GameScore {
+	
+	public int calculateScore(int correctCount, int incorrectCount) throws GameScoreExceptions{
+		return 0;
+	}
+
+}
