be728b8aae499246dc6ecb71b0c0e4f36fb3ee0c
Santiago Agustin Laiton Cubides <santiago.laiton@mail@escuelaing.edu.co>
2020-09-04 20:03:32 -0500

    injection

--- src/main/java/hangman/setup/guice/HangmanFactoryServices.java
+++ src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -5,17 +5,23 @@
  */
 package hangman.setup.guice;
 
+import hangman.model.English;
+
 /**
  *
  * @author 2106913
  */
 
 import hangman.model.French;
+import hangman.model.GameScore;
 import hangman.model.Language;
+import hangman.model.OriginalScore;
 import hangman.model.dictionary.HangmanDictionary;
+import hangman.model.dictionary.EnglishDictionaryDataSource;
 import hangman.model.dictionary.FrenchDictionaryDataSource;
 import hangman.view.HangmanNoviolentoPanel;
 import hangman.view.HangmanPanel;
+import hangman.view.HangmanStickmanPanel;
 
 public class HangmanFactoryServices extends com.google.inject.AbstractModule {
 
@@ -23,6 +29,11 @@ public class HangmanFactoryServices extends com.google.inject.AbstractModule {
     protected void configure() {
         /* Guice dependency injection */
         // bind(Interface.class).to(Concrete.class);
+    	bind(Language.class).to(English.class);
+    	bind(HangmanDictionary.class).to(EnglishDictionaryDataSource.class);
+    	bind(HangmanPanel.class).to(HangmanStickmanPanel.class);
+    	bind(GameScore.class).to(OriginalScore.class);
+    	
     }
 
 }
